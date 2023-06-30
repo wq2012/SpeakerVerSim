2abacef651182759411cb4e67d62dd0bc95954c7
Quan Wang <quanw@google.com>
2023-06-29 21:25:52 -0400

    Use unittest package

--- test_simulation.py
+++ test_simulation.py
@@ -1,3 +1,5 @@
+import unittest
+
 import server_single_simple
 import server_single_sync
 import server_single_hash
@@ -5,32 +7,34 @@ import server_single_multiprofile
 import server_double
 
 
-def test_server_single_simple():
-    stats = server_single_simple.main()
-    assert len(stats.final_messages) == 1080
+class TestServerSimulation(unittest.TestCase):
+
+    def test_server_single_simple(self):
+        stats = server_single_simple.main()
+        assert len(stats.final_messages) == 1080
+
+    def test_server_single_syncself(self):
+        stats = server_single_sync.main()
+        assert len(stats.final_messages) == 1080
+
+    def test_server_single_hash(self):
+        stats = server_single_hash.main()
+        assert len(stats.final_messages) == 1080
+        assert stats.backward_bounce_count == 0
+        assert stats.forward_bounce_count in {0, 1}
+
+    def test_server_single_multiprofile(self):
+        stats = server_single_multiprofile.main()
+        assert len(stats.final_messages) == 1080
+        assert stats.backward_bounce_count == 0
+        assert stats.forward_bounce_count in {0, 1}
+
+    def test_server_double(self):
+        stats = server_double.main()
+        assert len(stats.final_messages) == 1080
+        assert stats.backward_bounce_count == 0
+        assert stats.forward_bounce_count == 0
 
 
-def test_server_single_sync():
-    stats = server_single_sync.main()
-    assert len(stats.final_messages) == 1080
-
-
-def test_server_single_hash():
-    stats = server_single_hash.main()
-    assert len(stats.final_messages) == 1080
-    assert stats.backward_bounce_count == 0
-    assert stats.forward_bounce_count in {0, 1}
-
-
-def test_server_single_multiprofile():
-    stats = server_single_multiprofile.main()
-    assert len(stats.final_messages) == 1080
-    assert stats.backward_bounce_count == 0
-    assert stats.forward_bounce_count in {0, 1}
-
-
-def test_server_double():
-    stats = server_double.main()
-    assert len(stats.final_messages) == 1080
-    assert stats.backward_bounce_count == 0
-    assert stats.forward_bounce_count == 0
+if __name__ == "__main__":
+    unittest.main()
