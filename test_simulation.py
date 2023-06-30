350ac74ced6f9791a0b921a94452c93d30c539a2
Quan Wang <quanw@google.com>
2023-06-29 21:23:53 -0400

    Add test for server_double

--- test_simulation.py
+++ test_simulation.py
@@ -2,6 +2,7 @@ import server_single_simple
 import server_single_sync
 import server_single_hash
 import server_single_multiprofile
+import server_double
 
 
 def test_server_single_simple():
@@ -26,3 +27,10 @@ def test_server_single_multiprofile():
     assert len(stats.final_messages) == 1080
     assert stats.backward_bounce_count == 0
     assert stats.forward_bounce_count in {0, 1}
+
+
+def test_server_double():
+    stats = server_double.main()
+    assert len(stats.final_messages) == 1080
+    assert stats.backward_bounce_count == 0
+    assert stats.forward_bounce_count == 0
